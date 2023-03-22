cb874aa37c75494753966d205be701a4e309b242
Andrew <andrey.chulkov@list.ru>
2023-03-22 20:42:29 +0300

    Learn export and import

--- react/src/App.js
+++ react/src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
+import MyName from './MyName'
+import Li from './Li'
 
 function App() {
   return (
     <div>
       <MyName />
-      <ul className="p">
-        <li class="li">JS</li>
-        <li class="li">Html</li>
-        <li class="li">Name: <input min="0" type="number"></input></li>
-      </ul>
-    </div>
-  );
-}
-
-const MyName = () => {
-  return (
-    <div>
-      <p className="p">Hello world!</p>
-      <p className="p">My name Andrew</p>
+      <Li />
     </div>
   );
 }
---
+++ react/src/MyName.js
@@ -0,0 +1,10 @@
+const MyName = () => {
+    return (
+      <div>
+        <p className="p">Hello world!</p>
+        <p className="p">My name Andrew</p>
+      </div>
+    );
+  }
+
+export default MyName
