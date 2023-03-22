abb0903836e53822b530d22b6e2435cf0cd4067d
Andrew <andrey.chulkov@list.ru>
2023-03-22 20:16:32 +0300

    componenta

--- react/src/App.js
+++ react/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 function App() {
   return (
     <div>
-      <p className="p">Hello world!</p>
-      <p className="p">My name Andrew</p>
+      <MyName />
       <ul className="p">
         <li class="li">JS</li>
         <li class="li">Html</li>
-        <li class="li">Name: <input min = "0" type="number"></input></li>
-        </ul>      
+        <li class="li">Name: <input min="0" type="number"></input></li>
+      </ul>
+    </div>
+  );
+}
+
+const MyName = () => {
+  return (
+    <div>
+      <p className="p">Hello world!</p>
+      <p className="p">My name Andrew</p>
     </div>
   );
 }
