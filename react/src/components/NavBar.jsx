e05fcbb993ff94b0a4d2a0d1da19cc16464818f6
Andrew <andrey.chulkov@list.ru>
2023-03-23 16:21:09 +0300

    2

---
+++ react/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>Profile</li>
+                <li>Messages</li>
+                <li>News</li>
+                <li>Music</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar
---
+++ react/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg" />
+            </div>
+            <div>
+                Ava +
+            </div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+}
+
+export default Profile
