f7dd730a727e38332eda848319cd03ac8d1a1492
Andrew <andrey.chulkov@list.ru>
2023-03-25 16:23:22 +0300

    refactoring Messages and Profiles

--- react/src/components/Profiles/Profile.jsx
+++ react/src/components/Profiles/Profile.jsx
@@ -1,19 +1,15 @@
 import classes from './Profile.module.css'
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './MyPosts/ProfileInfo/ProfileInfo';
 
 
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img src="https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg" />
-            </div>
-            <div>
-                Ava +
-            </div>
+            <ProfileInfo />
             <MyPosts />
-                    </div>
+        </div>
     );
 }
 
