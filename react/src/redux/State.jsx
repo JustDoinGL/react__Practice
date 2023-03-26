3ebe2e72a99cd81d9e5cb17f78b62e9940fc1448
Andrew <andrey.chulkov@list.ru>
2023-03-26 15:28:30 +0300

    redux state.js

---
+++ react/src/redux/State.jsx
@@ -0,0 +1,23 @@
+const State = {
+    dialogData: [
+        { name: 'Andrew', id: '1' },
+        { name: 'Semen', id: '2' },
+        { name: 'Alex', id: '3' },
+        { name: 'Victor', id: '4' },
+        { name: 'Sacha', id: '5' },
+    ],
+
+    postsData: [
+        { id: 1, name: 'Andrew', text: 'Hello world!', valueLikes: '12' },
+        { id: 2, name: 'Alex', text: 'Hi react', valueLikes: '33' },
+        { id: 2, name: 'Alex', text: 'Hi react', valueLikes: '33' },
+    ],
+
+    messageData: [
+        { text: 'Hi', id: '1' },
+        { text: 'Semen', id: '2' },
+        { text: 'Alex', id: '3' },
+    ],
+}
+
+export default State
