82af019d4c22bc89d64a32e535b7b61dcd1868c4
Andrew <andrey.chulkov@list.ru>
2023-03-27 19:51:46 +0300

    fail onli

--- react/src/components/Profiles/MyPosts/MyPosts.jsx
+++ react/src/components/Profiles/MyPosts/MyPosts.jsx
@@ -1,7 +1,18 @@
 import Post from './Post/Post';
+import { React } from 'react';
+
 
 const MyPosts = (props) => {
+
     let outputPosts = props.postsData.map((element) => <Post text={element.text} name={element.name} valueLikes={element.valueLikes} />)
+
+    // this.infoTextarea = React.createRef();
+
+    let addPost = () => {
+        // let text = infoTextarea.current.value
+        alert("a")
+    }
+
     return (
         <div>
             <div>
@@ -9,8 +20,8 @@ const MyPosts = (props) => {
             </div>
             <div>
                 <p>New post: </p>
-                <textarea id="" ></textarea>
-                <button>Send</button>
+                <textarea></textarea>
+                <button onClick={addPost}>Send</button>
             </div>
             {outputPosts}
         </div>
