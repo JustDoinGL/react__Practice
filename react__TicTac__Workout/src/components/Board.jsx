540b77db4f601363d4edf9740660374b2bade42d
Andrew <andrey.chulkov@list.ru>
2023-03-30 15:47:49 +0300

    TicTac

--- react__TicTac__Workout/src/components/Board.jsx
+++ react__TicTac__Workout/src/components/Board.jsx
@@ -1,32 +1,26 @@
 import styles from './Board.module.css';
 import Square from './Square/Square';
-
+import Game from './Game/Game';
 
 
 function Board(props) {
-    function handleClick(i) {
-        const nextSquares = props.squares.slice();
-        nextSquares[i] = "X";
-        props.setSquares(nextSquares);
-    }
-
-
     return (
         <>
+            < Game />
             <div className={styles.board}>
-                <Square value={props.squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={props.squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={props.squares[2]} onSquareClick={() => handleClick(2)} />
+                <Square value={props.squares[0]} onSquareClick={() => props.handleClick(0)} />
+                <Square value={props.squares[1]} onSquareClick={() => props.handleClick(1)} />
+                <Square value={props.squares[2]} onSquareClick={() => props.handleClick(2)} />
             </div>
             <div className={styles.board}>
-                <Square value={props.squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={props.squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={props.squares[5]} onSquareClick={() => handleClick(5)} />
+                <Square value={props.squares[3]} onSquareClick={() => props.handleClick(3)} />
+                <Square value={props.squares[4]} onSquareClick={() => props.handleClick(4)} />
+                <Square value={props.squares[5]} onSquareClick={() => props.handleClick(5)} />
             </div>
             <div className={styles.board}>
-                <Square value={props.squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={props.squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={props.squares[8]} onSquareClick={() => handleClick(8)} />
+                <Square value={props.squares[6]} onSquareClick={() => props.handleClick(6)} />
+                <Square value={props.squares[7]} onSquareClick={() => props.handleClick(7)} />
+                <Square value={props.squares[8]} onSquareClick={() => props.handleClick(8)} />
             </div>
         </>
     );
