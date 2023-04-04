d8462077e199d9327da49d32d1c8bc68d79fe3a0
Andrew <andrey.chulkov@list.ru>
2023-04-04 21:11:10 +0300

    50:23

--- new/src/App.js
+++ new/src/App.js
@@ -2,28 +2,48 @@ import React, { useState } from "react";
 import "./styles/app.css";
 // import Counnter from "./components/Counter";
 // import ClassConter from './components/ClassCounter';
-// import Lists from './components/Lists/Posts/Lists';
+import Lists from "./components/Lists/Posts/Lists";
+import Button from "./UI/Button/Button";
+import Input from "./UI/Input/Input";
 
 function App() {
-//   const [posts, setPosts] = useState([
-//     { id: 1, name: "Js", text: "aaa" },
-//     { id: 2, name: "Js2", text: "aaa1111" },
-//     { id: 3, name: "Js2", text: "aasss222a" },
-//   ]);
+  const [posts, setPosts] = useState([
+    { id: 1, name: "Js", text: "aaa" },
+    { id: 2, name: "Js2", text: "aaa1111" },
+    { id: 3, name: "Js2", text: "aasss222a" },
+  ]);
 
-//   const [posts2, setPosts2] = useState([
-//     { id: 1, name: "XXXXXXX", text: "aaa" },
-//     { id: 2, name: "XXXXXX", text: "aaa1111" },
-//     { id: 3, name: "XXXX", text: "aasss222a" },
-//   ]);
+  // const [posts2, setPosts2] = useState([
+  //   { id: 1, name: "XXXXXXX", text: "aaa" },
+  //   { id: 2, name: "XXXXXX", text: "aaa1111" },
+  //   { id: 3, name: "XXXX", text: "aasss222a" },
+  // ]);
 
+  const [title, setTitile] = useState("");
+
+  function addNewPost(e) {
+    e.preventDefault();
+    console.log(title);
+  }
 
   return (
     <div className="App">
       {/* <Counnter />
-      <ClassConter />
+      <ClassConter /> */}
+      <form>
+        <Input
+          value={title}
+          onChange={(element) => {
+            setTitile(element.target.value);
+          }}
+          type="text"
+          placeholder="ВВедите название"
+        />
+        <Input type="text" placeholder="ВВедите текст" />
+        <Button onClick={addNewPost}>Сравнить</Button>
+      </form>
       <Lists title="js" post={posts} />
-      <Lists title="xxxs" post={posts2} /> */}
+      {/* <Lists title="xxxs" post={posts2} /> */}
     </div>
   );
 }
