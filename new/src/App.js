6824ac06566eb08ce30314817d04d128b636347c
Andrew <andrey.chulkov@list.ru>
2023-04-04 13:07:34 +0300

    24:07

--- new/src/App.js
+++ new/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState("");
 
   function on() {
     setLikes(likes + 1);
@@ -14,14 +15,15 @@ function App() {
     }
   }
 
-  
   function ziro() {
-    setLikes(likes-likes)
+    setLikes(likes - likes);
   }
 
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input value={value} onChange={element => setValue(element.target.value)} />
       <button onClick={on}>++++++++</button>
       <button onClick={out}>--------</button>
       <button onClick={ziro}>00000000000</button>
