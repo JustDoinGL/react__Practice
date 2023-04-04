fef3fffa939ef4a91e496862b7c302eeceef23fd
Andrew <andrey.chulkov@list.ru>
2023-04-04 18:09:59 +0300

    My practice with useState

--- new/src/App.js
+++ new/src/App.js
@@ -1,32 +1,15 @@
 import React from "react";
 import { useState } from "react";
+import Counnter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState("");
-
-  function on() {
-    setLikes(likes + 1);
-  }
-
-  function out() {
-    if (likes != 0) {
-      setLikes(likes - 1);
-    }
-  }
-
-  function ziro() {
-    setLikes(likes - likes);
-  }
+  const [value, setValue] = useState("U can change it");
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
       <h1>{value}</h1>
       <input value={value} onChange={element => setValue(element.target.value)} />
-      <button onClick={on}>++++++++</button>
-      <button onClick={out}>--------</button>
-      <button onClick={ziro}>00000000000</button>
+      <Counnter />
     </div>
   );
 }
