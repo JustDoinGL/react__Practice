c8c45dd14f8d133e2511e2964f7a7218d454c85f
Andrew <andrey.chulkov@list.ru>
2023-04-06 20:21:46 +0300

    1:22:00

--- new/src/components/Lists/Posts/Lists.jsx
+++ new/src/components/Lists/Posts/Lists.jsx
@@ -1,20 +1,29 @@
 import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-const Lists = ({info}) => {
+const Lists = ({ info }) => {
   if (!info.postSortedAnd.length) {
     return (
-      <h1 style={{ textAlign: "center", fontWeight: "22px" }}>
-        Посты не найдены!
-      </h1>
+      <TransitionGroup>
+        <CSSTransition key={info.postSortedAnd.id} timeout={500} classNames="post">
+          <h1 style={{ textAlign: "center", fontWeight: "22px" }}>
+            Посты не найдены!
+          </h1>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 
   return (
     <>
       <h1 style={{ textAlign: "center", fontSize: "44px" }}>{info.title}</h1>
-      {info.postSortedAnd.map((el) => (
-        <PostItem post={el} removePost={info.removePost} />
-      ))}
+      <TransitionGroup>
+        {info.postSortedAnd.map((el) => (
+          <CSSTransition key={el.id} timeout={500} classNames="post">
+            <PostItem post={el} removePost={info.removePost} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </>
   );
 };
