3d1a6c56155b33ef42de6001b4695e26494cfc24
Andrew <andrey.chulkov@list.ru>
2023-04-08 13:15:42 +0300

    2:18:00

--- new/src/App.js
+++ new/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import Pages from './Pages/Pages';
-import ReactDOM from "react-dom/client";
+import React from "react";
+import Pages from "./Pages/Pages";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Info from "./Pages/Info";
+import './styles/app.css'
 
 function App() {
   return (
-    <div><Pages/></div>
-  )
+    <BrowserRouter>
+    <div className="nav">
+        <div>
+          <Link className="navItem" to={`/pages`}>Pages</Link>
+          <Link className="navItem" to={`/info`}> Info </Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/pages" element={<Pages />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
