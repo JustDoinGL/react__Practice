f948762f90599d4cdaa3ef7c2fb6b265c610191e
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-04-10 21:26:21 +0300

    Delete new directory

--- new/src/Router/index.jsx
+++
@@ -1,7 +0,0 @@
-
-export const routes = [
-    {path:'/about', component : About, exact : true},
-    {path:'/about', component : About, exact : true},
-    {path:'/about', component : About, exact : true},
-    {path:'/about', component : About, exact : true},
-]
