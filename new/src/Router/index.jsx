1195a4443fa930788f3933efb143d62759efeb5c
Andrew <andrey.chulkov@list.ru>
2023-04-10 20:43:46 +0300

    -

---
+++ new/src/Router/index.jsx
@@ -0,0 +1,7 @@
+
+export const routes = [
+    {path:'/about', component : About, exact : true},
+    {path:'/about', component : About, exact : true},
+    {path:'/about', component : About, exact : true},
+    {path:'/about', component : About, exact : true},
+]
